46f2fc4e898e0217ad07b97e759c82df6a46dea8
BerkayAtakli <9023046@student.zadkine.nl>
2026-05-20 10:56:13 +0200

    ç

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php 
 try 
 { 
-    $db = new PDO('mysql:host=localhost;dbname=halalpork', 'root' ,'root'); 
+    $db = new PDO('mysql:host=localhost;dbname=halalpork', 'root' ,''); 
 } 
 catch(PDOException $e) 
 { 
