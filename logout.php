72208d07494567dfc02c6342c23f1bf37587e964
BerkayAtakli <9023046@student.zadkine.nl>
2026-06-17 13:08:03 +0200

    .

--- logout.php
+++ logout.php
@@ -1,7 +1,26 @@
 <?php
 session_start();
-$_SESSION = [];
-session_destroy();
-header('Location: index.php');
-exit();
+require_once 'product_helpers.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['confirm_logout'])) {
+    $_SESSION = [];
+    session_destroy();
+    header('Location: index.php');
+    exit();
+}
+
+render_header('Uitloggen');
 ?>
+<main class="centering">
+    <h2>Weet u zeker dat u zich wil afmelden?</h2>
+
+    <form action="logout.php" method="post" style="display:inline; margin-right: 10px;">
+        <input type="hidden" name="confirm_logout" value="1">
+        <button type="submit">Afmelden</button>
+    </form>
+
+    <form action="index.php" method="get" style="display:inline;">
+        <button type="submit">Breek af</button>
+    </form>
+</main>
+<?php render_footer(); ?>
